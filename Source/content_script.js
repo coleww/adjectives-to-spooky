676fd975f0a3aa373ca65e238957bb6ccd0170ad
Cole Willsea <coleww@gmail.com>
2015-10-18 14:28:47 -0700

    sweet starter there, yeah

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,19 +1,19 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -27,15 +27,41 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bHillary\b/gi, "");
+	v = v.replace(/\bClinton\b/gi, "");
+	v = v.replace(/\bBernie\b/gi, "");
+	v = v.replace(/\bSanders\b/gi, "");
+	v = v.replace(/\bTrump\b/gi, "");
+	v = v.replace(/\bDonald\b/gi, "");
+	v = v.replace(/\bJeb\b/gi, "");
+	v = v.replace(/\bBush\b/gi, "");
+	v = v.replace(/\bObama\b/gi, "");
+	v = v.replace(/\bBen Carson\b/gi, "");
+	v = v.replace(/\bMarco Rubio\b/gi, "");
+	v = v.replace(/\bTed Cruz\b/gi, "");
+	v = v.replace(/\bCarson\b/gi, "");
+	v = v.replace(/\bRubio\b/gi, "");
+	v = v.replace(/\bCruz\b/gi, "");
+
+
+
+
+
+	v = v.replace(/\bpoll/gi, "");
+	v = v.replace(/\bcampaign/gi, "");
+	v = v.replace(/\badminist/gi, "");
+	v = v.replace(/\bsupport/gi, "");
+	v = v.replace(/\bendorse/gi, "");
+	v = v.replace(/\bstump/gi, "");
+	v = v.replace(/\bcampaign/gi, "");
+	v = v.replace(/\badminist=/gi, "");
+	v = v.replace(/\bsupport/gi, "");
+	v = v.replace(/\bendorse/gi, "");
+
 	textNode.nodeValue = v;
 }
 
